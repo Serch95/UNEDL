864c8d2e242be73c4c5e76d1d0f1ed6b7eef1fc4
Serch95 <sherrera1695@gmail.com>
2018-10-14 20:43:37 -0500

    UNEDL

---
+++ Programas/Palindromo/Palindromo/Program.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Palindromo {
+    class Program {
+        static void Main(string[] args) {
+            String palindromo;
+            Console.WriteLine("Ingresa una palabra");
+            palindromo = Console.ReadLine();
+            if (Palindromo(palindromo) == true) {
+                Console.WriteLine("Si es un palindromo");
+            } else {
+                Console.WriteLine("No es un palindromo");
+            }
+            
+            Console.ReadKey();
+        }
+
+        public static bool Palindromo(string palindromo) {
+            palindromo = palindromo.Replace(" ","");
+            int min = 0;
+            int max = palindromo.Length - 1;
+            while (true) {
+                if (min > max) {
+                    return true;
+                }
+                char a = palindromo[min];
+                char b = palindromo[max];
+                if (char.ToLower(a) != char.ToLower(b)) {
+                    return false;
+                }
+                min++;
+                max--;
+            }
+        }
+
+    }
+}
